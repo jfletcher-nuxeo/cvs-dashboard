14aeaf206bde85ee2ac57c6502b303c6f16519cd
Josh Fletcher <jfletcher@nuxeo.com>
2016-01-08 18:11:20 -0800

    Fix dependencies.

--- cvs-dashboard-plugin/src/main/yo/Gruntfile.js
+++ cvs-dashboard-plugin/src/main/yo/Gruntfile.js
@@ -237,7 +237,7 @@ module.exports = function (grunt) {
             '*.html',
             'elements/**',
             '!elements/**/*.css',
-            'images/{,*/}*.{webp,gif}'
+            'images/**'
           ]
         }]
       },
